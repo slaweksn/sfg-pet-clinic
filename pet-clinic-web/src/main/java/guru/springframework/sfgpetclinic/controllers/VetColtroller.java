2d03d975d7c7f54aee4d58cbabef5c19c7ebbfac
slaweksn <slaweksnieg@gmail.com>
2022-12-29 21:37:46 +0100

    adding vets page and controller, closes #7.

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetColtroller.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class VetColtroller {
+
+	@GetMapping(path = {"vets", "vets/index"})
+	public String listVets() {
+		
+		return "vets/index";
+	}
+}
