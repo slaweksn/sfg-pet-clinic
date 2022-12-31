d5625a30a187646031de208262808fe981c262de
slaweksn <slaweksnieg@gmail.com>
2022-12-31 14:51:43 +0100

    adding owners list and update controller, closes #16.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -24,6 +24,7 @@ public class DataLoader implements CommandLineRunner {
 		// TODO Auto-generated method stub
 		
 		Owner owner1 = new Owner();
+		owner1.setId(1l);
 		owner1.setFirstName("Michael");
 		owner1.setLastName("Weston");
 		//owner1.setAddress("123 Brickerel");
@@ -32,6 +33,7 @@ public class DataLoader implements CommandLineRunner {
 		ownerService.save(owner1);
 		
 		Owner owner2 = new Owner();
+		owner2.setId(2l);
 		owner2.setFirstName("Fiona");
 		owner2.setLastName("Glenanne");
 		//owner2.setAddress("123 Brickerel");
@@ -42,6 +44,7 @@ public class DataLoader implements CommandLineRunner {
 		System.out.println("Loaded Owners....");
 		
 		Vet vet1 = new Vet();
+		vet1.setId(1l);
 		vet1.setFirstName("Sam");
 		vet1.setLastName("Axe");
 		//vet1.getSpecialities().add(savedRadiology);
@@ -49,6 +52,7 @@ public class DataLoader implements CommandLineRunner {
 		vetService.save(vet1);
 
 		Vet vet2 = new Vet();
+		vet2.setId(2l);
 		vet2.setFirstName("Jessie");
 		vet2.setLastName("Porter");
 		//vet2.getSpecialities().add(savedSurgery);
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,16 +1,31 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import java.util.Collection;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.sfgpetclinic.services.OwnerService;
 
 @Controller
 @RequestMapping(path = "owners")
 public class OwnerController {
 
+	private final OwnerService ownerService;
+	
+	public OwnerController(OwnerService ownerService) {
+		super();
+		this.ownerService = ownerService;
+	}
+	
 	@GetMapping(path = { "", "/", "/index" })
-	public String listOwners() {
+	public String listOwners(Model model) {
 
+		model.addAttribute("owners", ownerService.findAll().stream().sorted((o1,o2)->o1.getId().compareTo(o2.getId())).collect(Collectors.toList()));
+		
 		return "owners/index";
 	}
 }
