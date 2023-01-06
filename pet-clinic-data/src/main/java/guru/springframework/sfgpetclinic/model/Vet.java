39566e5ebb9ddb5c9683ab4457cc3fdd630ffd28
slaweksn <slaweksnieg@gmail.com>
2023-01-06 15:51:38 +0100

    added Speciality entity and update Vet relationship to Speciality, closes #28.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,5 +1,7 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
 	/**
@@ -7,4 +9,15 @@ public class Vet extends Person {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	private Set<Speciality> specialities;
+
+	public Set<Speciality> getSpecialities() {
+		return specialities;
+	}
+
+	public void setSpecialities(Set<Speciality> specialities) {
+		this.specialities = specialities;
+	}
+	
+	
 }
