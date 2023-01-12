db60e20896c6d5990f3b9e636c2858c7e72df469
slaweksn <slaweksnieg@gmail.com>
2023-01-12 16:51:38 +0100

    Update base entity with JPA annotions, closes #36

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,12 +1,21 @@
 package guru.springframework.sfgpetclinic.model;
 
 import java.io.Serializable;
+import java.time.Instant;
 
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 
-public class BaseEntity implements Serializable {
+@MappedSuperclass
+public abstract class BaseEntity implements Serializable {
 
 	/**
 	 * 
@@ -17,6 +26,22 @@ public class BaseEntity implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@CreatedBy
+	@Column(name = "created_by", nullable = false, length = 50, updatable = false)
+	private String createdBy;
+
+	@CreatedDate
+	@Column(name = "created_date", updatable = false)
+	private Instant createdDate = Instant.now();
+
+	@LastModifiedBy
+	@Column(name = "last_modified_by", length = 50)
+	private String lastModifiedBy;
+
+	@LastModifiedDate
+	@Column(name = "last_modified_date")
+	private Instant lastModifiedDate = Instant.now();
+	
 	public Long getId() {
 		return id;
 	}
@@ -24,5 +49,38 @@ public class BaseEntity implements Serializable {
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
+	//public abstract T getId();
+	
+	public String getCreatedBy() {
+        return createdBy;
+    }
 
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Instant getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Instant createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public Instant getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Instant lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
 }
