6b77d880095dc562e068879537b022861f1d2194
slaweksn <slaweksnieg@gmail.com>
2023-01-12 17:26:59 +0100

    updated Vet and Speciality to entity, closes #37.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -1,5 +1,11 @@
 package guru.springframework.sfgpetclinic.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="specialities")
 public class Speciality extends BaseEntity {
 
 	/**
@@ -7,6 +13,7 @@ public class Speciality extends BaseEntity {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	@Column(name="description")
 	private String description;
 
 	public String getDescription() {
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -3,6 +3,15 @@ package guru.springframework.sfgpetclinic.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="vets")
 public class Vet extends Person {
 
 	/**
@@ -10,6 +19,10 @@ public class Vet extends Person {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "vet_specialities",
+				joinColumns = @JoinColumn(name="vet_id"),
+				inverseJoinColumns = @JoinColumn(name="speciality_id"))
 	private Set<Speciality> specialities = new HashSet<>();
 
 	public Set<Speciality> getSpecialities() {
